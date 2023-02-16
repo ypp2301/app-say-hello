780d66b8df539a9da1ac2083f1f11d69db6dc46c
dhacr7 <yp021004@gmail.com>
2023-02-16 10:13:04 +0700

    rest API

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -41,3 +41,40 @@ func Create(c *gin.Context) {
 	models.DB.Create(&product)
 	c.JSON(http.StatusOK, gin.H{"products": product})
 }
+
+func Update(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+
+	if models.DB.Model(&product).Where("id = ?", id).Updates(&product).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "Tidak dapat mengupdate product"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"products": "Data berhasil diperbarui"})
+}
+
+func Delete(c *gin.Context) {
+	var product models.Product
+	id := c.Param("id")
+
+	//var input struct {
+	//	Id json.Number
+	//}
+
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+
+	//id, _ := input.Id.Int64()
+	if models.DB.Delete(&product, id).RowsAffected == 0 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Tidak dapat menghapus product"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"products": "Data berhasil dihapus"})
+}
